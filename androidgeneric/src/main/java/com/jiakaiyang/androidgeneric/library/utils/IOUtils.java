cf94dab7c9e339847f4a982b8bc2243741538b70
kaiyangjia@163.com <kaiyangjia@163.com>
2016-03-06 16:12:23 +0800

    reset project

---
+++ androidgeneric/src/main/java/com/jiakaiyang/androidgeneric/library/utils/IOUtils.java
@@ -0,0 +1,29 @@
+package main.java.com.jiakaiyang.androidgeneric.library.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by kaiyangjia on 2016/2/26.
+ */
+public class IOUtils {
+    public static String getStringFormStream(InputStream inputStream){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder result = new StringBuilder();
+        String line;
+
+        try {
+            while((line = reader.readLine()) != null){
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return "";
+        }
+
+        return result.toString();
+    }
+}
