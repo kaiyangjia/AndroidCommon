d2e4ba9c609b3eda69b5585987ad403753c01673
kaiyangjia@163.com <kaiyangjia@163.com>
2016-03-06 16:45:13 +0800

    reinit project

---
+++ androidcommon/src/main/java/com/jiakaiyang/androidcommon/androidcommon/utils/TypeUtils.java
@@ -0,0 +1,41 @@
+package com.jiakaiyang.androidcommon.androidcommon.utils;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kaiyangjia on 2016/1/29.
+ */
+public class TypeUtils {
+
+    /**
+     * 把一个bean对象转换成一个Map对象，返回的Map中key为Bean对象的成员名称，value为bean的该成员对应的值
+     * @param bean
+     * @return
+     */
+    public static Map getMapFromBean(Object bean){
+        Map result = new HashMap();
+
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for(Field entity : fields){
+            String name = entity.getName();
+            boolean access = entity.isAccessible();
+            if(!access){
+                entity.setAccessible(true);
+            }
+
+            try {
+                Object o = entity.get(bean);
+                result.put(name, o);
+                if(!access){
+                    entity.setAccessible(false);
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+        return result;
+    }
+}
---
+++ androidcommon/src/main/java/com/jiakaiyang/androidcommon/androidcommon/widget/DatePickerView.java
@@ -0,0 +1,58 @@
+package com.jiakaiyang.androidcommon.androidcommon.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.jiakaiyang.androidgeneric.library.R;
+import com.jiakaiyang.androidgeneric.library.utils.StringUtils;
+
+/**
+ * Created by jiakaiyang on 2015/12/23.
+ */
+public class DatePickerView extends FrameLayout {
+    private OnPickerButtonClickListener onPickerButtonClickListener;
+
+    public DatePickerView(Context context) {
+        super(context);
+    }
+
+    public DatePickerView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DatePickerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        LayoutInflater.from(context).inflate(R.layout.date_picker, this);
+        TypedArray t = context.obtainStyledAttributes(attrs, R.styleable.DatePickerView);
+        String title = t.getString(R.styleable.DatePickerView_view_title);
+        if(StringUtils.NotEmpty(title)){
+            TextView tvTitle = (TextView) findViewById(R.id.date_picker_title);
+            tvTitle.setText(title + "：");
+        }
+
+        findViewById(R.id.date_picker_btn).setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                onPickerButtonClickListener.OnPickerButtonClick(DatePickerView.this);
+            }
+        });
+    }
+
+    public void setOnPickerButtonClickListener(OnPickerButtonClickListener onPickerButtonClickListener) {
+        this.onPickerButtonClickListener = onPickerButtonClickListener;
+    }
+
+    public interface OnPickerButtonClickListener{
+        void OnPickerButtonClick(DatePickerView pickerView);
+    }
+
+    public void setDateSelectedText(String string){
+        ((TextView) findViewById(R.id.date_picker_string)).setText(string);
+    }
+}
